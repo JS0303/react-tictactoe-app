1907497697c9ded8bbfb0a8fbbd1cb4abf622e79
js0303 <wlstjr0303@gmail.com>
2023-02-16 14:18:27 +0900

    동작 기록 저장하기 구현 중..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   return (
     <div className="game">
       <div className="game-board">
