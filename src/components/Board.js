c071a0aaeb7f590819f5cd1a8aa9a3ad2ac6b09c
js0303 <wlstjr0303@gmail.com>
2023-02-16 11:16:40 +0900

    Square 컴포넌트 클릭 시 변화주기

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,8 +9,20 @@ export default class Board extends Component {
       squares: Array(9).fill(null),
     };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
